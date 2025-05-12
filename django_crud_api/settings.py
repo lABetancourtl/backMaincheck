090ee78f253bcdb0799051a5f589bddbc7781847
lABetancourtl <anderson.betancourta@uqvirtual.edu.co>
2025-05-12 15:15:11 -0500

    Actualización nuevo despliegue2

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRECT_KEY', default='asda4s654a7sd9as55ssd')
+SECRET_KEY = os.environ.get('SECRECT_KEY', default='asda4s654a7sd9as55ssd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
